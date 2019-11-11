c7f80d05366f012326d04a55ee1d665397ea330b
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2019-11-11 20:15:29 +0530

    Add files via upload

---
+++ Nov_long_1.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t-->0)
+		{int n=s.nextInt();
+		String c[]=new String[n];
+		for(int i=0;i<n;i++)
+		c[i]=s.next();
+		int ans=0;
+		for(int i=0;i<10;i++)
+		{int count=0;
+		    for(int j=0;j<n;j++)
+		{if(c[j].charAt(i)=='1')
+		count++;}
+		if(count%2==1)
+		ans++;}
+		    System.out.println(ans);}
+		}
+	}
